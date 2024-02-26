d046c962fbecf5aecefc655b0fbbb2684108d7b9
pasukanbocil <ctian8787@gmail.com>
2024-02-26 15:10:20 +0700

    Meli Amelia Putri

---
+++ ajaxapi/app.js
@@ -0,0 +1,12 @@
+const data = `{
+        "ticker": {
+        "base": "BTC",
+        "target": "IDR",
+        "price":"134124252.000000000",
+        "volume": "28.00000000",
+        "change": "-340000.00000000"
+         },
+        "timestamp": 1623360000,
+        "success": true,
+        "error": ""
+}`;
